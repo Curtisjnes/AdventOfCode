19a6b47ebe11c329ed3afaf0f054f69902241e31
Curtisjnes <curtisclajones@gmail.com>
2022-12-09 12:52:50 +0100

    The whole Puzzle gets solved in the Solution() Method, too lazy to clean up my Code, maybe some other day

--- src/CalorieCounting.java
+++ src/CalorieCounting.java
@@ -1,12 +1,89 @@
 import java.sql.SQLOutput;
-
+import java.util.*;
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
 public class CalorieCounting {
     Select select = new Select();
-    private String filePath = "C:\\Users\\cjones\\IdeaProjects\\AdventOfCodePuzzles\\src\\PuzzleInput\\CalorieCounting.txt";
+
+    List <Integer> sums = new ArrayList<>();
+    private String filePath = "C:\\Users\\cjones\\IdeaProjects\\AdventOfCodePuzzles\\src\\PuzzleInput\\CalorieCounting.txt"; //Ordnerpfad des Inputs
+
+    public CalorieCounting() throws IOException {
+    }
 
     public void start(){
-        select.PullAndSaveFile(filePath);
-        select.sumUntilCertainParameter(filePath, "", 0);
+        select.pullAndSaveFile(filePath); //Ruft die Methode auf, die den Input in einer Liste speichert und  gibt den Ordnerpfad mit
+        select.parseToIntegerList(); //Weil dieses Puzzle das verrechnen von Zahlen benötigt brauche ich eine Integer-Liste
+        System.out.println(select.getIntAllData()); //Output, um zu checken, ob die Methoden funktionieren
+        solution();
+        //solutionExtra();
+    }
+
+    private void solution(){ //meine Lösung für das Problem
+        /*
+        Meine Lösungs-Idee:
+        Ich ziehe mir die Integer-Liste und rechne alle Zahlen zusammen bis zur ersten Null. Speicher das Ergebnis in einer Integer-Liste.
+        Und dann kann ich im Idealfall einfach die max() funktion verwenden um mir den größten Wert auszugeben, damit sollte ich dann die Lösung für das Puzzle haben.
+         */
+
+        int index = 0;
+        int sum = 0;
+        for ( int i  = 0; i < select.getIntAllData().size(); i++){
+            select.writeToFile( "",index, sum, select.getIntAllData(),i, 1);
+            sum = sum + select.getIntAllData().get(i);
+            select.writeToFile("",index, sum, select.getIntAllData(),i, 2);
+            if (select.getIntAllData().get(i) == 0 ) { //Wenn in der liste Null steht, nimmt die Liste einfach die Null auf, weil null unmöglich das höchste Ergebnis sein kann
+                index = sum;
+                sum = 0;
+                sums.add(index);
+                System.out.println(index);
+            } else if (select.getIntAllData().get(i) == 0 && index > sum) {
+                sum = 0;
+            }
+        }
+        int max = Collections.max(sums);
+        Collections.sort(sums);
+        Collections.reverse(sums);
+        index = sums.get(0) + sums.get(1) + sums.get(2);
+        System.out.println(index + " " + max);
+
+    }
+
+    private void solutionExtra(){ //meine Lösung für das Problem
+        int [] topThree = new int[3];
+        boolean pushed = false;
+        int index = 0;
+        int sum = 0;
+        for ( int i  = 0; i < select.getIntAllData().size(); i++){
+            sum = sum + select.getIntAllData().get(i);
+            int indexArray = 0;
+            int cache = 0;
+            if (select.getIntAllData().get(i) == 0 && index < sum) {
+                for (int j = 0; j < topThree.length; j++){
+                    if (topThree[j] < sum && pushed == false){
+                        indexArray = j;
+                        cache = topThree[j];
+                        topThree[j] = sum;
+                        pushed = true;
+                        select.writeToFile("Pushed: ", sum, 0 , select.getIntAllData(), j, 3);
+                        if (topThree[j] != 0 ){
+                            select.writeToFile("Popped: ", indexArray, cache, select.getIntAllData(), j, 4);
+                            pushed = true;
+                            break;
+                        }
+                        break;
+                    }else {
+                        //select.writeToFile(" Nothing happened current values: " + " j: " + String.valueOf(j),cache, sum, select.getIntAllData(), topThree[j], 5 );
+
+                    }
+                    pushed = false;
+                }
+                sum = 0;
+            }
+        }
+        index = topThree[0] + topThree[1] + topThree[2];
+        System.out.println(index);
+
     }
 
 
--- src/Select.java
+++ src/Select.java
@@ -1,50 +1,73 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 import java.nio.file.*;
+import java.util.stream.Collectors;
 
 public class Select {
     private int length = 0;
-    public List<String> allData = new ArrayList<String>();
+    public File myObj = null;
+    private  FileWriter myWriter = new FileWriter("PushToArray.txt");
 
-    private String [] lastIndexBeforeInput = new String[10000];
+    public Select() throws IOException {
+    }
 
-    String lastInputBevoreParam = null;
-    int j = 0;
-    private String fileAsString = null;
+    public List<String> getAllData() {
+        return allData;
+    }
 
-    public void PullAndSaveFile(String file){
+    private List<String> allData = new ArrayList<String>();
+
+    public List<Integer> getIntAllData() {
+        return intAllData;
+    }
+
+    private List<Integer> intAllData = new ArrayList<>();
+
+
+    //Zieht den Input aus der .txt datei und speichert alles in einer Liste
+    public void pullAndSaveFile(String file) {
         try {
             List<String> allLines = Files.readAllLines(Paths.get(file));
 
-            for (String line : allLines) {
-                //System.out.println(line);
-                allData.add(line);
+            allData.addAll(allLines);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void writeToFile(String action, int index, int sum, List<Integer> intAllData, int i, int indicator){
+        try {
+            if (indicator == 1) {
+                myWriter.write("Folgendes Ergebnis wurde Berechnet: " + String.valueOf(sum) + " + " + String.valueOf(intAllData.get(i)) + " = ");
+            } else if (indicator == 3) {
+                myWriter.write( action + String.valueOf(index) + " to ArrayIndex:" + String.valueOf(i));
+                myWriter.write(System.getProperty( "line.separator" ));
+            } else if (indicator == 4) {
+                myWriter.write(action + String.valueOf(sum) + " from ArrayIndex: " + String.valueOf(i));
+                myWriter.write(System.getProperty( "line.separator" ));
+            } else if (indicator == 5) {
+                myWriter.write(action + " Cache " + String.valueOf(index) + " sum " + String.valueOf(sum)  + " topThree[i] " + String.valueOf(i));
+                myWriter.write(System.getProperty( "line.separator" ));
+            } else {
+                myWriter.write( " " + String.valueOf(sum));
+                myWriter.write(System.getProperty( "line.separator" ));
             }
         } catch (IOException e) {
+            System.out.println("An error occurred.");
             e.printStackTrace();
         }
     }
 
-    public void sumUntilCertainParameter(String file, String character, int number) {
-            int index = 0;
-        if (number == 0 && character != null) {
-            for (String i : allData) {
-                j++;
-                if (i.equals(character)) {
-                    lastInputBevoreParam = String.valueOf(allData.get(j - 2));
-                    lastIndexBeforeInput[index] = allData.get(j-2);
-                    System.out.println(lastIndexBeforeInput[index]);
-                    index++;
-                }
-            }
-        } else {
-            System.out.println("Wird sich drum gekümmert");
-        }
 
-
+    //Falls die Werte Zahlen sind, die man zusammenrechnen muss, kann man mithilfe dieser Methode den Input in eine Int-Liste schreiben
+    public void parseToIntegerList(){
+        Collections.replaceAll(allData, "", "0");
+        intAllData = allData.stream().map(Integer::parseInt).collect(Collectors.toList());
     }
-
 }
