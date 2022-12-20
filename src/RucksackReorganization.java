f94831718423eced43bbda28a4d5e400fa8a9217
curtisjnes <Curtisclajones@gmail.com>
2022-12-20 13:53:30 +0100

    First Part of Puzzle successfully completed

--- src/RucksackReorganization.java
+++ src/RucksackReorganization.java
@@ -1,48 +1,65 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class RucksackReorganization {
     Select s = new Select();
-    char [] alphabet = "abcdefghijklmopqrstuvwxyz".toCharArray();
     String filePath = "C:\\Users\\cjones\\IdeaProjects\\AdventOfCodePuzzles\\src\\PuzzleInput\\RucksackReorganization\\RRInput.txt";
     List<String> firstCompartment = new ArrayList<>();
+    int addprio = 0;
+    boolean exit = false;
     List<String> secondCompartment = new ArrayList<>();
-
-    private boolean compareAsciiDez(int first, int second){
-        boolean compareAscii = false;
-
-
-        return compareAscii;
-    }
+    List <String> alphabet = Arrays.asList(" ","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
     public void start(){
         s.pullAndSaveFile(filePath);
 
         for(int i = 0; i < s.getAllData().size(); i++){
-            char [] split = s.getAllData().get(i).toCharArray();
-            for(int s = 0; s < split.length; s++){
-                if (s < split.length/2){
-                    firstCompartment.add(String.valueOf(split[s]));
-                } else if (s >= split.length/2) {
-                    secondCompartment.add(String.valueOf(split[s]));
-                }
-            }
-
-            for (int j = 0; j < firstCompartment.size(); j++){
-                if (firstCompartment.contains(String.valueOf(secondCompartment.get(j)))){
-                    char c = secondCompartment.get(j).charAt(0);
-                    int index = firstCompartment.indexOf(String.valueOf(c));
-                    char x = firstCompartment.get(index).charAt(0);
-                    if (Character.isUpperCase(c) && Character.isUpperCase(x) || Character.isLowerCase(c) && Character.isLowerCase(x)){
-                        System.out.println("beide listen haben " + c + " " + x + " gemeinsam");
+            if (i % 3 ==0 ) {
+                char[] split = s.getAllData().get(i).toCharArray();
+                for (int s = 0; s < split.length; s++) {
+                    if (s < split.length / 2) {
+                        firstCompartment.add(String.valueOf(split[s]));
+                    } else if (s >= split.length / 2) {
+                        secondCompartment.add(String.valueOf(split[s]));
                     }
                 }
             }
+            for (String value : secondCompartment) {
+
+                if (secondCompartment.contains(String.valueOf(value)) && firstCompartment.contains(String.valueOf(value))) {
+                    char c = value.charAt(0);
+                    int index = firstCompartment.indexOf(String.valueOf(c));
+                    boolean b = Character.isUpperCase(c) && Character.isUpperCase(firstCompartment.get(index).charAt(0)) && !exit;
+                    boolean b1 = Character.isLowerCase(c) && Character.isLowerCase(firstCompartment.get(index).charAt(0)) && !exit;
+
+                    if (b || b1) {
+                        if (b) {
+                            char l = Character.toLowerCase(c);
+                            addprio = addprio + alphabet.indexOf(String.valueOf(l)) + 26;
+                            exit = true;
+                        } else {
+                            int m = alphabet.indexOf(String.valueOf(c));
+                            addprio = addprio + alphabet.indexOf(String.valueOf(c));
+                            exit = true;
+                        }
+                    }
+
+
+                }
+
+
+            }
+
+
             firstCompartment.clear();
             secondCompartment.clear();
-            System.out.println("new Input");
+
+            exit = false;
         }
+        System.out.println(addprio);
+
     }
 
     public RucksackReorganization() throws IOException {
